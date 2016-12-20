83b9a46df705da2d7e67e4e162100bc3165cfe1a
arjunmahishi <arjun.mahishi@gmail.com>
2016-12-21 01:50:21 +0530

    Fixed upload news module. Working!

--- app/src/main/java/com/instify/android/upload/UploadNews.java
+++ app/src/main/java/com/instify/android/upload/UploadNews.java
@@ -1,12 +1,16 @@
 package com.instify.android.upload;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.instify.android.R;
@@ -40,9 +44,23 @@ public class UploadNews extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (validateForm()){
+                    // TODO: Use a relevant key for the news in the database, attach user details.
+
                     CampusNewsData data = new CampusNewsData(newsTitle, newsDescription);
                     Timestamp tStamp = new Timestamp(System.currentTimeMillis());
-                    campusNewsRef.child(tStamp.toString()).setValue(data);
+                    campusNewsRef.child("" + tStamp.getTime()).setValue(data)
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(UploadNews.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(UploadNews.this, "News announced!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                 }
             }
         });
